a2ee55565884317b1e72617dd9c2c7295ce98d0a
unknown <Daniel>
2025-03-23 23:47:34 -0400

    resources page styling + start on js

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ var courses = [["SC MATH 1019 Discrete Math",
 
 function findCourse() {
     var error = document.getElementById("error");
+    var searchBar = document.getElementById("searchBar").innerHTML;
+    var flag = 0;
+    for (i = 0; i <= courses.length; i++) {
 
+    }
+    // In order for search to accuratly find, there are 3 major flags:
+    // Course code
+    // Course name
+    error.innerHTML = indexed;
     error.innerHTML = "Error: Course Not Found";
 }
